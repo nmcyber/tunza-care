42ebbbe985bc19bc98a7d53d63c36ca0ed13436c
xanderyeng <alexander.chepkiyeng@gmail.com>
2025-03-24 01:52:39 +0300

    Feat: Added an Applications Form with a Hybrid approach. A Dialog window and a full page view mode.

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import ContactPage from './pages/Contact';
 import PrivacyPolicy from './pages/Privacy-Policy';
 import TermsConditions from './pages/Terms-Conditions';
 import ScrollToTop from './utlis/ScrollToTop';
+import ApplicationPage from './pages/Applications';
 
 function App() {
   return (
@@ -21,6 +22,8 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/employment" element={<EmploymentPage />} />
+        <Route path="/careers" element={<ApplicationPage />} />
+        <Route path="/careers/apply" element={<ApplicationPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms-conditions" element={<TermsConditions />} />
